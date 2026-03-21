86b8449d57092310b71bfab36dbab2253fb20522
kin412 <kiwo412@gmail.com>
2026-03-21 16:38:04 +0900

    ui css 스타일 적용

--- next/section02/src/pages/index.tsx
+++ next/section02/src/pages/index.tsx
@@ -1,16 +1,29 @@
 //import "./index.css"; //넥스트는 이렇게 하면 오류남. _app.tsx 컴포넌트에서는 최상위부모이기 때문에 가능.
 //css module을 사용해야함.
+//@ 경로는 src임. tsconfig.json에 설정되어있음
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Home() {
   //return <h1 style={{ color: "red" }}>인덱스</h1>;
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
--- next/section02/src/pages/search/index.tsx
+++ next/section02/src/pages/search/index.tsx
@@ -3,18 +3,25 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 //page폴더 자체가 최상위 요청경로다. 스프링의 requestmapping("/")
 //search폴더에 index를 만들면 만든 것 만으로 localhost:3000/search 요청 시 index파일을 호출한다.
 export default function Page() {
-  const router = useRouter();
-
-  console.log(router);
-  //쿼리스트링 꺼내오기
-  //const q = router.query.q;
-  const { q } = router.query;
-
-  return <h1>search q: {q}</h1>;
+  // const router = useRouter();
+  // console.log(router);
+  // //쿼리스트링 꺼내오기
+  // //const q = router.query.q;
+  // const { q } = router.query;
+  // return <h1>search q: {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
