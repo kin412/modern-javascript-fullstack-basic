d28eab3c80ad5234cf0392c1d155dd93573ebaa5
kin412 <kiwo412@gmail.com>
2026-03-23 15:50:05 +0900

    데이터 캐시

--- next/section03/next.config.ts
+++ next/section03/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //데이터 페칭 로그
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
--- next/section03/src/app/(with-searchbar)/page.tsx
+++ next/section03/src/app/(with-searchbar)/page.tsx
@@ -5,6 +5,7 @@ import { BookData } from "@/types";
 async function AllBooks() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }, //캐싱되지 않음. 매요청마다 새롭게 불러옴 -> 15버전부터는 이게 기본 옵션임.
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
@@ -24,6 +25,15 @@ async function AllBooks() {
 async function RecoBooks() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    //항상 캐싱 됨. 처음 백엔드서버에서 응답이오면 해당 데이터를 set해놓고 있다가
+    //브라우저로부터 다시 요청이 올경우 next서버에서 백엔드까지 가지않고 set해놨던 데이터를 hit시켜서 브라우저로 보냄
+    //그래서 한번 호출된 이후에는 다시는 백엔드서버를 호출하지 않음
+    //{ cache: "force-cache" },
+
+    //기본적으로 force-cache 와 동일하나 지정한 시간이 지난경우
+    //일단 요청이 들어오면 기존있던 set해놨던 데이터는 stale(상했다.)상태이기때문에 일단 그거라도 화면에 보내서 빠르게 응답하고
+    //stale은 그대로 둘순 없으니 다시 백엔드 서버에서 데이터를 가져와 신선한 데이터를 set하고 다음 응답 부터 최신 데이터로 응답
+    { next: { revalidate: 3 } },
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다.</div>;
