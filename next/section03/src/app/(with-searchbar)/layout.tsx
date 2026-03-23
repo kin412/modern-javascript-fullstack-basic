2b3c5f489792f908d4b2214e3f55b3b34c6e8673
kin412 <kiwo412@gmail.com>
2026-03-23 19:13:38 +0900

    클라이언트 라우터 캐시

--- next/section03/src/app/(with-searchbar)/layout.tsx
+++ next/section03/src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,8 @@ import Searchbar from "../../components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* 클라이언트 라우터 캐시의 예 */}
+      {/* <div>{new Date().toLocaleDateString()}</div> */}
       {/* ⚠️ 여기서 확실하게 감싸줘야 / 페이지 빌드 에러가 사라집니다! */}
       <Suspense fallback={<div>Loading...</div>}>
         <Searchbar />
