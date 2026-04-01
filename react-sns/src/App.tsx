c170a71967cccd338f1358091fd9d1a64700fa1a
kin412 <kiwo412@gmail.com>
2026-04-01 20:43:22 +0900

    tailwindcss 설치 및 설정

--- react-sns/src/App.tsx
+++ react-sns/src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="bg-black text-2xl font-bold underline">hello world</div>
+  );
 }
 
 export default App;
