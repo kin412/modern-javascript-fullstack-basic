8897448db88e75421ef826826cfc05978b68581b
kin412 <kiwo412@gmail.com>
2026-04-02 21:34:20 +0900

    json-server 세팅

--- react-sns/vite.config.ts
+++ react-sns/vite.config.ts
@@ -11,4 +11,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  //json-server로 설정한 로컬테스트db서버가 변해도 리렌더링 되지않음.
+  server: {
+    watch: {
+      ignored: ["**/server/**"],
+    },
+  },
 });
