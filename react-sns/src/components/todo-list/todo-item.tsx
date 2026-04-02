46def72a52e082b92303991b45afda043559a79a
kin412 <kiwo412@gmail.com>
2026-04-02 20:50:34 +0900

    zustand todolist

---
+++ react-sns/src/components/todo-list/todo-item.tsx
@@ -0,0 +1,23 @@
+import { useDeleteTodo } from "@/store/todo";
+import { Button } from "../ui/button";
+
+export default function TodoItem({
+  id,
+  content,
+}: {
+  id: number;
+  content: string;
+}) {
+  const deleteTodo = useDeleteTodo();
+  const handleDeleteClick = () => {
+    deleteTodo(id);
+  };
+  return (
+    <div className="flex items-center justify-between border p-2">
+      {content}
+      <Button variant={"destructive"} onClick={handleDeleteClick}>
+        삭제
+      </Button>
+    </div>
+  );
+}
