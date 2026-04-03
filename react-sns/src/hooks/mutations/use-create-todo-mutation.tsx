b41d86410b2616972d0ae0fc90aa60f22789aa5a
kin412 <kiwo412@gmail.com>
2026-04-03 20:13:50 +0900

    tanstack query - 캐시 데이터 직접 수정 - setQueryData

--- react-sns/src/hooks/mutations/use-create-todo-mutation.tsx
+++ react-sns/src/hooks/mutations/use-create-todo-mutation.tsx
@@ -1,5 +1,6 @@
 import { createTodo } from "@/api/create-todo";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
@@ -10,13 +11,18 @@ export function useCreateTodoMutation() {
     onMutate: () => {}, //이벤트 핸들러 - 요청 시작시
     onSettled: () => {}, //요청 종료시
     //요청 성공시
-    onSuccess: () => {
+    onSuccess: (newTodo) => {
       //window.location.reload();
 
       //todos 캐시 무효화 - 데이터 리페칭
-      queryClient.invalidateQueries({
-        //queryKey: ["todos"],
-        queryKey: QUERY_KEYS.todo.list,
+      //   queryClient.invalidateQueries({
+      //     //queryKey: ["todos"],
+      //     queryKey: QUERY_KEYS.todo.list,
+      //   });
+      // 데이터 리페칭 없이 캐시 데이터 직접 수정
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
     },
     //요청 실패시
