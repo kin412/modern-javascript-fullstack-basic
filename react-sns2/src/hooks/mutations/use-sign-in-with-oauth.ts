0938dd6ac1998f4c33890a76cad2851ef04e4778
kin412 <kiwo412@gmail.com>
2026-04-08 22:48:45 +0900

    인증 에러 처리

--- react-sns2/src/hooks/mutations/use-sign-in-with-oauth.ts
+++ react-sns2/src/hooks/mutations/use-sign-in-with-oauth.ts
@@ -1,8 +1,12 @@
 import { signInWithOAuth } from "@/api/auth";
+import type { useMutationCallback } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
-export function useSignInWIthOAuth() {
+export function useSignInWIthOAuth(callbacks?: useMutationCallback) {
   return useMutation({
     mutationFn: signInWithOAuth,
+    onError: (error) => {
+      if (callbacks?.onError) callbacks.onError(error);
+    },
   });
 }
--- react-sns2/src/hooks/mutations/use-sign-in-with-password.ts
+++ react-sns2/src/hooks/mutations/use-sign-in-with-password.ts
@@ -1,8 +1,16 @@
 import { signInWithPassword } from "@/api/auth";
+import type { useMutationCallback } from "@/types";
 import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-export function useSignInWithPassword() {
+export function useSignInWithPassword(callbacks?: useMutationCallback) {
   return useMutation({
     mutationFn: signInWithPassword,
+    // mutationFn에서 던진 error가 onError로 옴
+    onError: (error) => {
+      console.error(error);
+
+      if (callbacks?.onError) callbacks.onError(error);
+    },
   });
 }
