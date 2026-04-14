be687999b861fe65d3de6f334f1c9464ba17db66
kin412 <kiwo412@gmail.com>
2026-04-14 16:33:20 +0900

    cud 캐시 최적화

--- react-sns2/src/hooks/mutations/post/use-delete-post.ts
+++ react-sns2/src/hooks/mutations/post/use-delete-post.ts
@@ -1,17 +1,25 @@
 import { deleteImagesInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { useMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: useMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (deletedPost) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
 
       //이미지를 삭제하는 기능
-      if (deletedPost.image_urls && deletedPost.image_urls.length > 0)
+      if (deletedPost.image_urls && deletedPost.image_urls.length > 0) {
         await deleteImagesInPath(`${deletedPost.author_id}/${deletedPost.id}`);
+      }
+
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
