ef08829fb10df3ba79616070b55d1f3281a24c06
kin412 <kiwo412@gmail.com>
2026-04-14 16:05:02 +0900

    캐시 정규화

--- react-sns2/src/hooks/queries/use-infinite-posts-data.ts
+++ react-sns2/src/hooks/queries/use-infinite-posts-data.ts
@@ -1,10 +1,12 @@
 import { fetchPosts } from "@/api/post";
 import { QUERY_KEYS } from "@/lib/constants";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 
 const PAGE_SIZE = 5;
 
 export function useInfinitePostsData() {
+  const queryClient = useQueryClient();
+
   return useInfiniteQuery({
     queryKey: QUERY_KEYS.post.list,
     queryFn: async ({ pageParam }) => {
@@ -12,7 +14,13 @@ export function useInfinitePostsData() {
       const to = from + PAGE_SIZE - 1;
 
       const posts = await fetchPosts({ from, to });
-      return posts;
+      //캐시 정규화
+      posts.forEach((post) => {
+        queryClient.setQueryData(QUERY_KEYS.post.byid(post.id), post);
+      });
+      //리턴하는게 queryKey의 캐시데이터, 그러므로 캐시 정규화를 위해 수정
+      //return posts;
+      return posts.map((post) => post.id);
     },
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) => {
