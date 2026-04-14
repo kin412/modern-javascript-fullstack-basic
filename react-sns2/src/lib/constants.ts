63dc051fe27752daf4af6981c8cfb04fb0ecdbd8
kin412 <kiwo412@gmail.com>
2026-04-14 20:42:04 +0900

    프로필 페이지 구현

--- react-sns2/src/lib/constants.ts
+++ react-sns2/src/lib/constants.ts
@@ -7,6 +7,7 @@ export const QUERY_KEYS = {
   post: {
     all: ["post"],
     list: ["post", "list"],
+    userList: (userId: string) => ["post", "userList", userId],
     byid: (postId: number) => ["post", "list", postId],
   },
 };
