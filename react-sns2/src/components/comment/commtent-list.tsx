68fcd18c47b780cbdff40ae89b2b021aec411244
kin412 <kiwo412@gmail.com>
2026-04-15 00:53:20 +0900

    대댓글 기능 구현

--- react-sns2/src/components/comment/commtent-list.tsx
+++ react-sns2/src/components/comment/commtent-list.tsx
@@ -2,6 +2,28 @@ import CommentItem from "@/components/comment/comment-item";
 import { useCommentsData } from "@/hooks/queries/use-comments-data";
 import Fallback from "../fallback";
 import Loader from "../loader";
+import type { Comment, NestedComment } from "@/types";
+import { Children } from "react";
+
+function toNestedComments(comments: Comment[]): NestedComment[] {
+  const result: NestedComment[] = [];
+
+  comments.forEach((comment) => {
+    if (!comment.parent_comment_id) {
+      result.push({ ...comment, children: [] });
+    } else {
+      const parentCommnetIndex = result.findIndex(
+        (item) => item.id === comment.parent_comment_id,
+      );
+      result[parentCommnetIndex].children.push({
+        ...comment,
+        children: [],
+        parentComment: result[parentCommnetIndex],
+      });
+    }
+  });
+  return result;
+}
 
 export default function CommentList({ postId }: { postId: number }) {
   const {
@@ -13,9 +35,11 @@ export default function CommentList({ postId }: { postId: number }) {
   if (fetchCommentError) return <Fallback />;
   if (isFetchCommentsPending) return <Loader />;
 
+  const nestedComments = toNestedComments(comments);
+
   return (
     <div className="flex flex-col gap-5">
-      {comments.map((comment) => (
+      {nestedComments.map((comment) => (
         <CommentItem key={comment.id} {...comment} />
       ))}
     </div>
