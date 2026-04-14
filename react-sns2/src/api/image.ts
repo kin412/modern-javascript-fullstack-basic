4a452924990f13f55a8abeac570f633ca9d391ec
kin412 <kiwo412@gmail.com>
2026-04-14 22:02:49 +0900

    프로필 수정 기능 구현 - 기능

--- react-sns2/src/api/image.ts
+++ react-sns2/src/api/image.ts
@@ -23,6 +23,11 @@ export async function deleteImagesInPath(path: string) {
   const { data: files, error: fetchFilesError } = await supabase.storage
     .from(BUCKET_NAME)
     .list(path);
+
+  if (!files || files.length === 0) {
+    return;
+  }
+
   if (fetchFilesError) throw fetchFilesError;
   const { error: removeError } = await supabase.storage
     .from(BUCKET_NAME)
